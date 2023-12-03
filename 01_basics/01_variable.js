480365085ce17ff1ce8a2b88740e2ac5ff409abb
SABINA <152700039+SABINA0910@users.noreply.github.com>
2023-12-03 14:20:48 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 9989
+
+let accountEmail = "hasme@gmail.com"
+var accountCity = "Kathmandu"
+accountPassword = "8989"
+let accountState;
+
+console.log(accountId);
+console.log(accountCity);
+
+accountCity = "Biratnagar"
+accountEmail = "hchc4@google.com"
+accountPassword = "0909"
+
+console.table([accountCity,accountEmail,accountId,accountState,accountPassword]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert (3 + 3) // we are using nodejs ,not the browser
+
+// code readeability should be high
+
+//let name = "sabina"  //this is string datatype
+//let age = 25 // this is a number variable
+//let isLoggedIn = True/False //this is a a boolean 
+
+// number => 2 to power 53 range
+// bigint = this is for the largest numbers for large co
+
+//string => "" 
+// boolean => true/false
+// null => standalone value null means empty but it can be assign as null
+// undefined => means when you do not assign any value
+// symbol => unique
+
+// object
+
+
+let name = "sabina"
+console.log(typeof "sabina");  // so the ouput will be string
+
+let age = 25
+console.log(typeof age);
+
+console.log(typeof null); //here null is an object 
+console.log(typeof undefined); // output will be undefined only
